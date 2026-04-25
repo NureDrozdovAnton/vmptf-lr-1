fb7da30c45baefddcddcce20d31ac7ced9c174c1
Anton <anton.drozdov@nure.ua>
2026-04-25 15:04:26 +0300

    Fix filter condition

--- currency/services.py
+++ currency/services.py
@@ -18,6 +18,7 @@ def get_active_exchange_rates(at=None):
     uah = Currency.objects.get(code='UAH')
     rates = ExchangeRate.objects.filter(
         Q(ended_at__isnull=True) | Q(ended_at__gt=now),
+        Q(started_at__lte=now),
         from_currency=uah,
     ).select_related('to_currency').order_by('to_currency__code')
 
